340839b2273ea7c7edc64e07c673475b11c09507
Ben Reeves <zootreeves@gmail.com>
2012-01-19 12:37:55 +0000

    JQuery cookie sucks

--- shared.js
+++ shared.js
@@ -253,14 +253,12 @@ function setAdv(isOn) {
 }
 
 function toggleSymbol() {
-	$.cookie('local', null);
-
 	if (symbol === symbol_btc) {
 		symbol = symbol_local;
-		$.cookie('local', 'true');
+		SetCookie('local', 'true');
 	} else { 	
 		symbol = symbol_btc;
-		$.cookie('local', 'false');
+		SetCookie('local', 'false');
 	}
 	
 	$('span[data-c]').each(function(index) {
@@ -287,35 +285,19 @@ $(document).ready(function() {
 	} catch (e) {}
 });
 
-jQuery.cookie = function (key, value, options) {
 
-    // key and at least value given, set cookie...
-    if (arguments.length > 1 && String(value) !== "[object Object]") {
-        options = jQuery.extend({}, options);
-
-        if (value === null || value === undefined) {
-            options.expires = -1;
-        }
-
-        if (typeof options.expires === 'number') {
-            var days = options.expires, t = options.expires = new Date();
-            t.setDate(t.getDate() + days);
-        }
-
-        value = String(value);
-
-        return (document.cookie = [
-            encodeURIComponent(key), '=',
-            options.raw ? value : encodeURIComponent(value),
-            options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
-            options.path ? '; path=' + options.path : '',
-            options.domain ? '; domain=' + options.domain : '',
-            options.secure ? '; secure' : ''
-        ].join(''));
-    }
-
-    // key and possibly options given, get cookie...
-    options = value || {};
-    var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;
-    return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;
-};
+function SetCookie() {
+if(arguments.length < 2) { return; }
+var n = arguments[0];
+var v = arguments[1];
+var d = 0;
+if(arguments.length > 2) { d = parseInt(arguments[2]); }
+var exp = '';
+if(d > 0) {
+	var now = new Date();
+	then = now.getTime() + (d * 24 * 60 * 60 * 1000);
+	now.setTime(then);
+	exp = '; expires=' + now.toGMTString();
+	}
+document.cookie = n + "=" + escape(String(v)) + '; path=/' + exp;
+}
