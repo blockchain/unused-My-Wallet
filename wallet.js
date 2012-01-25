5147d3311d4a92751184b338db015fcaf317c257
Ben Reeves <zootreeves@gmail.com>
2012-01-25 09:47:11 +0000

    After enabling dropbox automatically do backup

--- wallet.js
+++ wallet.js
@@ -2726,9 +2726,10 @@ function bind() {
 	
 	$('#wallet-dropbox-enabled').change(function(e) {	
 		var val = 'false';
+	
 		if ($(this).is(':checked')) {
 			val = 'true';
-			setTimeout(doWalletBackup, 1000); //Delayed backup (let setting be updated first)
+			setTimeout(backupWallet, 1000); //Delayed backup (let setting be updated first)
 		}
 		
 		updateKV('Updating Dropbox Settings', 'update-dropbox-enabled', val);
