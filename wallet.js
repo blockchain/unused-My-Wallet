f3419799de4a4709dd242ac3be6919c990a9aaa3
Ben Reeves <zootreeves@gmail.com>
2012-05-05 04:02:43 +0100

    Fix Error pages

--- wallet.js
+++ wallet.js
@@ -4521,7 +4521,7 @@ $(document).ready(function() {
 	
 	if (window.location.protocol == 'http:') {
 		makeNotice('error', 'add-error', 'You must use https:// not http://. Please update your link', 0);
-		//return;
+		return;
 	}
 	
 	if (!isSignup) {
