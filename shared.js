668b7121c2e9e35d984b33203d1947c3ed0ac6b9
Ben Reeves <zootreeves@gmail.com>
2012-12-11 13:22:09 +0000

    Changes to Zaypay Approval, NewWallet add both compressed and uncompressed keys

--- shared.js
+++ shared.js
@@ -11,8 +11,9 @@ var war_checksum;
 
 
 $.fn.center = function () {
-    this.css("top", Math.max(( $(window).height() - this.height() ) / 2+$(window).scrollTop(), 10) + "px");
-    this.css("left", Math.max(( $(window).width() - this.width() ) / 2+$(window).scrollLeft(), 10) + "px");
+    $(window).scrollTop();
+    this.css("top", Math.max(($(window).height() / 2) - (this.height() / 2), 10) + "px");
+    this.css("left", Math.max(($(window).width() / 2) - (this.width() / 2), 10) + "px");
     return this;
 };
 
