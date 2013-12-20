170b02862d77e779647d47c2bfa23f5e0a163425
Ben Reeves <zootreeves@gmail.com>
2013-12-20 11:55:53 +0000

    Fix Sharedcoin change problem

--- qr.code.reader.js
+++ qr.code.reader.js
@@ -116,7 +116,11 @@ var QRCodeReader = {
         loadScript('wallet/llqrcode', function() {
 
             // Standard and prefixed methods for hooking into stream
-            navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.msGetUserMedia;
+            navigator.getUserMedia = navigator.getUserMedia ||
+                navigator.webkitGetUserMedia ||
+                navigator.mozGetUserMedia ||
+                navigator.msGetUserMedia;
+
             window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
 
             QRCodeReader.reader_container = el.find('.qr-code-reader');
