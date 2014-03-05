959c14dbcd0288bed55e6e2729c276676c8550da
Tim Lee <tim@blockchain.info>
2014-03-05 18:41:38 +0000

    rename create account button

--- blockchain-com-mobile.js
+++ blockchain-com-mobile.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
     //Chrome should automatically grant notification permissions
     MyWallet.setHTML5Notifications(true);
 
-    $('#create-account').click(function() {
+    $('#create-account-btn').click(function() {
         Mobile.loadTemplate('create-account')
     });
 });
