f127c12e731466f9747a46a080ae13a03a0c1639
Tim Lee <tim@blockchain.info>
2014-03-06 22:59:15 +0000

    add parsePairingCode

--- blockchain-com-mobile.js
+++ blockchain-com-mobile.js
@@ -347,5 +347,84 @@ var Mobile = new function() {
             }
         });
     }
-
 }
+
+
+function loadTemplatetransactions() {
+    var body = $(document.body);
+
+    var data_root = body.data('root');
+    if (data_root)
+        root = data_root;
+
+    var data_resource = body.data('resource');
+    if (data_resource)
+        resource = data_resource;
+
+    //Chrome should automatically grant notification permissions
+    MyWallet.setHTML5Notifications(true);
+
+    Mobile.loadTemplate('transactions');
+}
+
+function parsePairingCode(raw_code) {
+
+    var success = function(pairing_code) {
+        device.execute("didParsePairingCode:", pairing_code);
+    }
+
+    var error = function(message) {
+        device.execute("errorParsingPairingCode:", message);
+    }
+
+    try {
+        if (raw_code == null || raw_code.length == 0) {
+            throw "Invalid Pairing QR Code";
+        }
+
+        var components = raw_code.split("|");
+
+        if (components.length < 3) {
+            throw "Invalid Pairing QR Code. Not enough components.";
+        }
+
+        var guid = components[1];
+        if (guid.length != 36) {
+            throw "Invalid Pairing QR Code. GUID wrong length.";
+        }
+
+        var encrypted_data = components[2];
+
+        $.ajax({
+            type: "POST",
+            url: root + 'wallet',
+            data : {format : 'plain', method : 'pairing-encryption-password', guid : guid},
+            success: function(encryption_phrase) {
+
+                var decrypted = MyWallet.decrypt(encrypted_data, encryption_phrase, MyWallet.getDefaultPbkdf2Iterations(), function(decrypted) {
+                    return decrypted != null;
+                }, function() {
+                    error('Decryption Error');
+                });
+
+                if (decrypted != null) {
+                    var components2 = decrypted.split("|");
+
+                    success({
+                        version : raw_code[0],
+                        guid : guid,
+                        sharedKey : components2[0],
+                        password : UTF8.bytesToString(Crypto.util.hexToBytes(components2[1]))
+                    });
+                }
+            },
+            error : function(res) {
+                error(res.responseText);
+            }
+        });
+
+    } catch (e) {
+        error('Error ' + e);
+    }
+}
+
