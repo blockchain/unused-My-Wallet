d8d720026ebeffd7c6ec114ef9eeaf2519310357
Tim Lee <tim@blockchain.info>
2014-03-20 15:57:40 -0700

    use col-xs-3 for mobile only

--- wallet.js
+++ wallet.js
@@ -3404,9 +3404,12 @@ var MyWallet = new function() {
             return;
 
         if (cVisible != null) {
-            if ($('#' + cVisible.attr('id') + '-btn').length > 0)
-                $('#' + cVisible.attr('id') + '-btn').parent().attr('class', 'col-xs-3 text-center');
-
+            if ($('#' + cVisible.attr('id') + '-btn').length > 0) {
+                if (isMobile)
+                    $('#' + cVisible.attr('id') + '-btn').parent().attr('class', 'col-xs-3 text-center');
+                else
+                    $('#' + cVisible.attr('id') + '-btn').parent().attr('class', '');
+            }
             cVisible.hide();
         }
 
@@ -3415,7 +3418,10 @@ var MyWallet = new function() {
         cVisible.show();
 
         if ($('#' + cVisible.attr('id') + '-btn').length > 0)
-            $('#' + cVisible.attr('id') + '-btn').parent().attr('class', 'active col-xs-3 text-center');
+            if (isMobile)
+                $('#' + cVisible.attr('id') + '-btn').parent().attr('class', 'active col-xs-3 text-center');
+            else
+                $('#' + cVisible.attr('id') + '-btn').parent().attr('class', 'active');
 
         buildVisibleView(true);
     }
