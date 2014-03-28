e58a31fd206cd28e8b38e46c9ba7bbe177a9e2e5
Tim Lee <tim@blockchain.info>
2014-03-27 19:16:40 -0700

    move sendEvent did_decrypt

--- wallet.js
+++ wallet.js
@@ -2584,7 +2584,7 @@ var MyWallet = new function() {
 
         logout_timeout = setTimeout(MyWallet.logout, MyWallet.getLogoutTime());
 
-        //MyWallet.sendEvent('did_decrypt');
+        MyWallet.sendEvent('did_decrypt');
 
         MyStore.get('multiaddr', function(multiaddrjson) {
             if (multiaddrjson != null) {
@@ -2616,8 +2616,6 @@ var MyWallet = new function() {
 
         //We have dealt the the hash values, don't need them anymore
         window.location.hash = '';
-
-        MyWallet.sendEvent('did_decrypt');
     }
 
     function checkWalletChecksum(payload_checksum, success, error) {
