b76faa0f1cc19ede758d88bd92717ec83e222f83
Tim Lee <tim@blockchain.info>
2014-04-01 21:31:39 -0700

    fix no loading transactions list on browser with no data

--- wallet.js
+++ wallet.js
@@ -91,14 +91,6 @@ var MyWallet = new function() {
         return isIOSDevice;
     }
 
-    this.setIsMobile = function(val) {
-        isMobile = val;
-    }
-
-    this.getIsMobile = function() {
-        return isMobile;
-    }
-
     var wallet_options = {
         pbkdf2_iterations : default_pbkdf2_iterations, //Number of pbkdf2 iterations to default to for second password and dpasswordhash
         fee_policy : 0,  //Default Fee policy (-1 Tight, 0 Normal, 1 High)
@@ -2153,6 +2145,7 @@ var MyWallet = new function() {
         }
 
         var buildSome = function() {
+            // hasBuiltTransactionsListOnce use to avoid loading twice on pair device on html5 mobile
             if (! isMobile ||
                 ! hasBuiltTransactionsListOnce ||
                 isLazyLoadingTransactions) {
