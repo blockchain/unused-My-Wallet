33c1e0ec667e60fe68d4a0c27c8b2367b916bb07
Mark Pfluger <mark@pfluegerworld.de>
2014-12-11 10:55:27 -0800

    Export loadAdverts function so it can be overwritten for iOS wallet

--- wallet.js
+++ wallet.js
@@ -1826,7 +1826,7 @@ var MyWallet = new function() {
         });
     }
 
-    function loadAdverts(obj) {
+    this.loadAdverts= function(obj) {
         $.ajax({
             type: "GET",
             dataType: 'json',
