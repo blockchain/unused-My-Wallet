a9437ce9b363bd3654a06b5e360f2aab009f6d57
Ben Reeves <zootreeves@gmail.com>
2015-01-04 19:51:59 +0000

    Homepage block links point to /block

--- homepage.js
+++ homepage.js
@@ -62,7 +62,7 @@ webSocketConnect(function(ws) {
             else
                 var n_tx = 0;
 
-            $('<tr id="bi:'+block.blockIndex+'"><td><div><a href="'+root+'block-index/'+block.blockIndex+'/'+block.hash+'">'+block.height+'</a></div></td><td data-time="'+block.time+'"><div>< 1 minute</div></td><td class="hidden-phone"><div>'+block.txIndex.length+'</div></td><td class="hidden-phone"><div>'+formatMoney(block.totalBTCSent, true)+'</div></td><td><div>'+foundByTxt+'</div></td><td class="hidden-phone"><div>'+parseInt(block.size / 1024)+'</div></td></tr>').insertAfter($('#blocks tr:first')).find('div').hide().slideDown('slow');
+            $('<tr id="bi:'+block.blockIndex+'"><td><div><a href="'+root+'block/'+block.hash+'">'+block.height+'</a></div></td><td data-time="'+block.time+'"><div>< 1 minute</div></td><td class="hidden-phone"><div>'+block.txIndex.length+'</div></td><td class="hidden-phone"><div>'+formatMoney(block.totalBTCSent, true)+'</div></td><td><div>'+foundByTxt+'</div></td><td class="hidden-phone"><div>'+parseInt(block.size / 1024)+'</div></td></tr>').insertAfter($('#blocks tr:first')).find('div').hide().slideDown('slow');
 
             $('#blocks tr:last-child').remove();
         }
